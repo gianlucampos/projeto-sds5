07a9a0ea5acf4337379e4b05391fe23772747c0f
gianlucampos <gianlucampos97@gmail.com>
2021-11-03 12:26:09 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/repository/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsvendas.repository;
+
+import com.devsuperior.dsvendas.entities.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+}
---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dsvendas.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SaleService {
+}
